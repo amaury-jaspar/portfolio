6ba8c15472269dcb0cc80fea93c616e3c769d6ce
amaury-jaspar <a.jaspar@protonmail.com>
2019-05-07 14:02:33 +0200

    cleanup

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    // Sidenav
+    $('.sidenav').sidenav();
+
+    // ScrollSpy
+    $('.scrollspy').scrollSpy();
+
+    // Dropdown menus
+    $('.modal').modal();
+    $('.dropdown-trigger').dropdown({
+        hover: true,
+        coverTrigger: false,
+        constrainWidth: false
+    });
+
+    // Sticky top navigation bar
+    $(window).scroll(function() {
+        if ($('#profile').is(":visible")) {
+            if ($(window).scrollTop()>160) {
+                $('nav').addClass('sticky-nav');
+                $('#first-header').addClass('spaced-title');  // Adjustment to space out the first header from the top bar
+            } else {
+                $('nav').removeClass('sticky-nav');
+                $('#first-header').removeClass('spaced-title');
+            }
+        } else {
+            $('nav').addClass('sticky-nav');
+            $('#first-header').addClass('spaced-title');
+        }
+    });
+});
